3218dacdeb819763844ab497134677bb884cdd96
Olga Pearce <pearce8@llnl.gov>
2011-07-08 15:19:44 -0700

    newer version of mpi_utils

--- adept-utils/mpi_utils.h
+++ adept-utils/mpi_utils.h
@@ -1,43 +1,20 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2010, Lawrence Livermore National Security, LLC.  
-// Produced at the Lawrence Livermore National Laboratory  
-// Written by Todd Gamblin, tgamblin@llnl.gov.
-// LLNL-CODE-417602
-// All rights reserved.  
-// 
-// This file is part of Nami. For details, see http://github.com/tgamblin/nami.
-// Please also read the LICENSE file for further information.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are
-// permitted provided that the following conditions are met:
-// 
-//  * Redistributions of source code must retain the above copyright notice, this list of
-//    conditions and the disclaimer below.
-//  * Redistributions in binary form must reproduce the above copyright notice, this list of
-//    conditions and the disclaimer (as noted below) in the documentation and/or other materials
-//    provided with the distribution.
-//  * Neither the name of the LLNS/LLNL nor the names of its contributors may be used to endorse
-//    or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-// LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-/////////////////////////////////////////////////////////////////////////////////////////////////
 #ifndef MPI_UTILS_H
 #define MPI_UTILS_H
+///
+/// @file mpi_utils.h
+/// @brief Overloaded utility functions to convert between arbitrary C/C++ 
+///        types and MPI types, custom typedefs for cstdlib types like size_t,
+///        and a wrapper for MPI_Pack_Size
+///
 
-#include <stdint.h>
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
+#include <stdint.h>
 #include <mpi.h>
 
-/// Overloaded function for getting corresponding MPI types for C types.
+//
+// Overloaded functions for getting corresponding MPI types for C types.
+//
 inline MPI_Datatype mpi_typeof(char)                       {return MPI_CHAR;}
 inline MPI_Datatype mpi_typeof(signed short)               {return MPI_SHORT;}
 inline MPI_Datatype mpi_typeof(signed int)                 {return MPI_INT;}
@@ -55,21 +32,32 @@ inline MPI_Datatype mpi_typeof(std::pair<double,int>)      {return MPI_DOUBLE_IN
 inline MPI_Datatype mpi_typeof(std::pair<long double,int>) {return MPI_LONG_DOUBLE_INT;}
 inline MPI_Datatype mpi_typeof(std::pair<short,int>)       {return MPI_SHORT_INT;}
 
-
-// helper routine for packing functions below.
-// TODO: this really shouldn't call exit()
-inline int mpi_packed_size(int count, MPI_Datatype type, MPI_Comm comm) {
-  int size;
-  if (PMPI_Pack_size(count, type, comm, &size)) {
-    std::cerr << "Error getting size!" << std::endl;
-    exit(1);
-  }
-  return size;
-}
-
-/// Handy datatype for stdlib datatypes
+// 
+// Handy datatypes for stdlib datatypes
+// @todo these won't work in heterogeneous environments, as it's tied to the machine type.
+// 
 #define MPI_SIZE_T      (mpi_typeof(size_t()))
 #define MPI_INTPTR_T    (mpi_typeof(intptr_t()))
 #define MPI_UINTPTR_T   (mpi_typeof(uintptr_t()))
+
+///
+/// Expression-ifies the overly C-ish MPI_Pack_size function.  Just returns 
+/// the size instead of requring a temporary.
+///
+inline int mpi_packed_size(int count, MPI_Datatype type, MPI_Comm comm) {
+  int size;
+  MPI_Pack_size(count, type, comm, &size);
+  return size;
+}
+
+///
+/// PMPI binding for mpi_packed_size().
+///
+inline int pmpi_packed_size(int count, MPI_Datatype type, MPI_Comm comm) {
+  int size;
+  PMPI_Pack_size(count, type, comm, &size);
+  return size;
+}
+
 
 #endif // MPI_UTILS_H
