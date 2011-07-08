36b824540e212e3b9023aa590821f42aec130b19
Olga Pearce <pearce8@llnl.gov>
2011-07-08 11:54:07 -0700

    adding more utils in their own namespaces

--- adept-utils/io_utils.cpp
+++ adept-utils/io_utils.cpp
@@ -5,7 +5,7 @@
 // LLNL-CODE-417602
 // All rights reserved.  
 // 
-// This file is part of Libra. For details, see http://github.com/tgamblin/libra.
+// This file is part of Io_Utils. For details, see http://github.com/tgamblin/nami.
 // Please also read the LICENSE file for further information.
 // 
 // Redistribution and use in source and binary forms, with or without modification, are
@@ -29,76 +29,52 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /////////////////////////////////////////////////////////////////////////////////////////////////
-#include "matrix_utils.h"
+#include <cassert>
 
-#include <fstream>
-#include <iostream>
+#include "io_utils.h"
+
 using namespace std;
 
-namespace matrix_utils {
+namespace io_utils {
 
-  bool read_matrix(const char *filename, boost::numeric::ublas::matrix<double>& mat) {
-    ifstream file(filename);
-    if (file.fail()) return false;
-    
-    int cols = 0;
-    int rows = 0;
-    char c;
-    
-    // count cols on first line (assume same for following lines)
-    file.get(c);
-    while (file.good() && c != '\n') {
-      while (file.good() && c != '\n' && c == ' ') {
-        file.get(c);
+  size_t vl_write(ostream& out, unsigned long long size) {
+    int out_bytes = 0;
+
+    do {
+      unsigned long long out_byte = (long long)(size & 0x7Fll);
+      if ((size >>= 7) > 0) out_byte |= 0x80;
+
+      char oc = (char)out_byte;
+      out.write(&oc, 1);
+
+      if (!out.good()) {
+        cerr << "Error: can't write to file." << endl;
+        exit(1);
       }
-      if (c != '\n') {
-        cols++;
-      }
-      while (file.good() && c != '\n' && c != ' ') {
-        file.get(c);
-      }
-    }
-    
-    if (!file.good()) return false;
-    
-    // count remaining lines
-    while (file.good()) {
-      if (c == '\n') rows++;
-      file.get(c);
-    }
-    
-    file.close();  // close
-    file.clear();  // reset flags
-    file.open(filename);   // open at beginning.
-    
-    mat.resize(rows, cols);
-    string line;
-    for (int row=0; row < rows; row++) {
-      getline(file, line);
-      int pos = 0;
-      
-      for (int col=0; col < cols; col++) {
-        int start = line.find_first_not_of(" ", pos);
-        int end = line.find_first_of(" ", start);
-        mat(row,col) = strtod(&line[start], NULL);
-        pos = end;
-      }
-    }
-    
-    return true;
+      out_bytes++;
+    } while (size);
+
+    return out_bytes;
   }
   
   
-  bool isDivisibleBy2(size_t n, int level) {
-    while (level != 0) {
-      if (n & ((size_t)0x1)) {
-        return false;
-      }
-      n >>= 1;
-      level--;
-    }
-    
-    return true; 
+  unsigned long long vl_read(istream& in) {
+    unsigned shift = 0;
+    unsigned long long long_bytes = 0;
+
+    char ichar;
+    unsigned long long file_byte = 0;
+
+    // read variable-length header with number of code bytes
+    do {
+      in.read(&ichar, 1);
+      file_byte = (file_byte & ~0xFF) | ichar;
+      if (!in.good()) return 0;
+
+      long_bytes |= (long long)(file_byte & 0x7Fll) << shift;
+      shift += 7;
+    } while (file_byte & 0x80);
+
+    return long_bytes;
   }
-  
-}//namespace
+} //namespace
